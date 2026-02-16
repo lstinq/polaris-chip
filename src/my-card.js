3f4907bc8411ad28253b7f0e5544476ec288a34d
Mandy Liu <ikhiiofls@gmail.com>
2026-02-15 21:44:06 -0500

    Enhance my-card component with dynamic features and styling options.

--- src/my-card.js
+++ src/my-card.js
@@ -15,14 +15,32 @@ export class MyCard extends LitElement {
   constructor() {
     super();
     this.title = "My card";
+    this.fancy = false;
+    this.image = "";
+    this.alt = "";
+    this.fancy = false;
+  }
+
+  openChanged(e) {
+    if (e.target.hasAttribute('open')) {
+      this.fancy = true;
+    } else {
+      this.fancy = false;
+    }
   }
 
   static get styles() {
     return css`
+    :host {
+      display: block;
+    }
+    :host([fancy]) .card {
+      background-color: #dddddd
+    }
     .card {
       border: 4px solid black;
       display: inline-block;
-      padding: 8px;
+      padding: 8px; 
       margin: 16px;
       max-width: 400px;
       background-color: #FFFFFF;
@@ -44,22 +62,23 @@ export class MyCard extends LitElement {
       margin-top: 16px;
       margin-bottom: 16px;
     }
-    .card-details {
-      font-size: 24px;
+    details summary {
+      text-align: left;
+      font-size: 16px;
+      padding: 8px 0;
+      cursor: pointer;
+    }
+
+    details[open] summary {
       font-weight: bold;
-      color: black;
-      background-color: #dadada;
     }
-    .controls button {
-      color: black;
-      font-size: 24px;
-      display: inline-block;
+
+    details div {
+      border: 2px solid black;
+      text-align: left;
       padding: 8px;
-      margin: 16px;
-      background-color: white;
-    }
-    .alt-bg {
-      background-color: #D1DDF3;
+      height: 70px;
+      overflow: auto;
     }
     @media (min-width: 501px) and (max-width: 800px) {
       .card-details {
@@ -78,118 +97,32 @@ export class MyCard extends LitElement {
   }
 
   render() {
-    return html`
-    <div id="cardlist">
+  return html`
       <div class="card">
         <img class="card-image"
-            src="https://m.media-amazon.com/images/S/pv-target-images/87ecb1079ad2b165f69882328719eb72df6cbff76ea3b5a42d21c9ef733d5690.jpg"
-            alt="This is an alternative description."
+            src="${this.image}"
+            alt="${this.alt}"
         />
         <h2 class="card-title">
-          My Journey to You (2023)
+          ${this.title}
         </h2>
-        <p class="card-description">
-          The series tells the story of Yun Wei Shan, a spy longing for freedom, who infiltrates the Gong residence to complete a mission.
-          In the eerie and treacherous Gong residence, she encounters love and friendship, embarks on a journey of self-discovery, and finds the determination to move forward.
-          Together with the rebellious nobleman Gong Zi Yu, they grow and mature through their shared experiences.
-        </p>
-        <a href="https://example.com" class="card-details">Details</a>
-      </div>
-    </div>
-    
-        <div id="cardlist">
-      <div class="card">
-        <img class="card-image"
-            src="https://images.travelandleisureasia.com/wp-content/uploads/sites/3/2025/02/17153051/the-white-olive-tree-1-1600x900.jpeg"
-            alt="This is an alternative description."
-        />
-        <h2 class="card-title">
-          The White Olive Tree (2025)
-        </h2>
-        <p class="card-description">
-          During a mission in the East Country, reporter Song Ran is saved from danger by Li Zan, a Chinese explosive engineer.
-          As they grow closer, they find their ideals and souls align, sparking a romantic connection.
-          However, a bombing attack disrupts their budding relationship.
-          After returning to China, they face personal struggles and lose contact.
-          Li Zan suffers from guilt over his friend's death, while Song Ran faces controversy over a photo she took during the attack.
-          By chance, they reunite, and together, they heal and plant a white olive tree, symbolizing hope.
-        </p>
-        <a href="https://example.com" class="card-details">Details</a>
-      </div>
-    </div>
 
-        <div id="cardlist">
-      <div class="card">
-        <img class="card-image"
-            src="https://img.tvb.com/youku/prd/20251206/053500006843FE61203CC710A4B54066_615_346_20251206075652.jpg"
-            alt="This is an alternative description."
-        />
-        <h2 class="card-title">
-          The Seven Relics of Ill Omen (2025)
-        </h2>
-        <p class="card-description">
-          In ancient times, a mysterious substance descended upon Fengzi Ridge.
-          Capable of parasitic invasion, it twisted the human heart and lured its host towards evil.
-          Now, these relics of omen have reemerged, unleashing a series of bizarre and gruesome murders.
-          Five ordinary yet passionate young people (Luo Ren, Mu Dai, Yi Wan San, Yan Hong Sha, and Cao Yan Hua) are drawn into this crisis through their own fateful encounters.
-          They form the "Phoenix Squad" and embark on a perilous quest to subdue these seven omens.
-          Through a long and arduous journey fraught with danger, the five overcome relentless challenges, and their friendship is tested and deepened in the fires of adversity.
-        </p>
-        <a href="https://example.com" class="card-details">Details</a>
+        <details ?open="${this.fancy}" @toggle="${this.openChanged}">
+          <summary>Learn more</summary>
+          <div>
+            <slot></slot>
+          </div>
+        </details>
       </div>
-    </div>
-
-        <div id="cardlist">
-      <div class="card">
-        <img class="card-image"
-            src="https://m.media-amazon.com/images/M/MV5BNmVkZjdjNmMtMjliZC00ZTVkLWIxNDktZDlmYmU4OTEzODY4XkEyXkFqcGc@._V1_.jpg"
-            alt="This is an alternative description."
-        />
-        <h2 class="card-title">
-          Twelve Letters (2025)
-        </h2>
-        <p class="card-description">
-          It follows the mysterious and emotional journey of Tang Yi Xun and his lover, Ye Hai Tang.
-          When Hai Tang vanishes without warning, Yi Xun discovers a letter waiting in a mailbox.
-          He writes back and realizes they are not only separated by distance but also by time.
-          Across twelve letters spanning thirty-five years, their connection endures as they search for truth and hope, weaving together love, longing, and the challenges of fate.
-        </p>
-        <a href="https://example.com" class="card-details">Details</a>
-      </div>
-    </div>
-
-        <div id="cardlist">
-      <div class="card">
-        <img class="card-image"
-            src="https://media.themoviedb.org/t/p/w780/wxz9DOg9wMhkl4E2F3CjwZWSYS9.jpg"
-            alt="This is an alternative description."
-        />
-        <h2 class="card-title">
-          Lighter and Princess (2022)
-        </h2>
-        <p class="card-description">
-          Li Xun, a top scorer and talented programmer, is uninterested in typical college life, causing friction with his classmates.
-          Zhu Yun, a friendly and obedient young woman, meets Li Xun and is conflicted by his disruptive nature, yet feels drawn to him.
-          As they start to grow closer, a life-altering incident threatens to tear them apart, testing their budding romance. 
-        </p>
-        <a href="https://example.com" class="card-details">Details</a>
-      </div>
-    </div>
-
-    <div class="controls">
-      <button class="duplicate">Clone card</button>
-      <button class="title">Change title</button>
-      <button class="img-change">Change image</button>
-      <button id="bg-change">Change background</button>
-      <button id="delete">Delete card</button>
-    </div>`;
+    `;
   }
 
   static get properties() {
     return {
-      title: { type: String },
-      image: { type: String },
-      alt : { type: String },
+      title: { type: String, reflect: true },
+      image: { type: String, reflect: true },
+      alt : { type: String, reflect: true },
+      fancy: { type: Boolean, reflect: true }
     };
   }
 }
